5f863084a5c8d2e036313ffa2e86a9a8d413a294
hamid <hamid.khosravian@ut.ac.ir>
2018-04-21 17:30:33 +0430

    add views

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -9,6 +9,7 @@ module Api
         param! :birthday, String, blank: false, required: true
 
         @profile = Profile.new
+        @profile.user = current_user
         @profile.first_name = params["first_name"]
         @profile.last_name = params["last_name"]
         @profile.birthday = params["birthday"]
@@ -20,6 +21,8 @@ module Api
 
       def update
         @profile = current_user.profile
+        raise ProfileNotFoundError, I18n.t('messages.profile.not_found') unless @profile
+
         @profile.first_name = params["first_name"] if params["first_name"]
         @profile.last_name = params["last_name"] if params["last_name"]
         @profile.birthday = params["birthday"] if params["birthday"]
@@ -31,14 +34,16 @@ module Api
 
       def show
         @profile = current_user.profile
+        raise ProfileNotFoundError, I18n.t('messages.profile.not_found') unless @profile
+
         render 'api/v1/profile/show'
       end
 
       def user_profile
         param! :uuid, String, blank: false, required: true
 
-        @profile = User.find_by(uuid: params["uuid"])
-        raise ProfileNotFoundError, I18n.t('api.profile.not_found') unless @profile
+        @profile = User.find_by(uuid: params["uuid"]).profile
+        raise ProfileNotFoundError, I18n.t('messages.profile.not_found') unless @profile
         render 'api/v1/profile/show'
       end
     end
--- app/views/api/v1/profile/show.jbuilder
+++ app/views/api/v1/profile/show.jbuilder
@@ -1,5 +1,4 @@
-byebug
-if current_user.profile == @profile or current_user.admin?
+if @current_user.profile == @profile or @current_user.admin?
   json.partial! 'api/v1/_partials/profile/complate', profile: @profile
 else
   json.partial! 'api/v1/_partials/profile/show', profile: @profile
