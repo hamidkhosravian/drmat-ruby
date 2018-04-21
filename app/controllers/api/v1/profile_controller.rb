e3e806a774246671edc7f6b96b89c412f22a2480
hamid <hamid.khosravian@ut.ac.ir>
2018-04-21 15:23:46 +0430

    add profile controller methods

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -1,18 +1,45 @@
 module Api
   module V1
     class ProfileController < ApiController
-      before_action :authenticate_user_from_token!, except: [:index]
+      before_action :authenticate_user_from_token!
 
       def create
+        param! :first_name, String, blank: false, required: true
+        param! :last_name, String, blank: false, required: true
+        param! :birthday, String, blank: false, required: true
+
+        @profile = Profile.new
+        @profile.first_name = params["first_name"]
+        @profile.last_name = params["last_name"]
+        @profile.birthday = params["birthday"]
+        @profile.email = params["email"]
+        @profile.save!
+
+        render 'api/v1/profile/show'
       end
 
       def update
+        @profile = current_user.profile
+        @profile.first_name = params["first_name"] if params["first_name"]
+        @profile.last_name = params["last_name"] if params["last_name"]
+        @profile.birthday = params["birthday"] if params["birthday"]
+        @profile.email = params["email"] if params["email"]
+        @profile.save!
+
+        render 'api/v1/profile/show'
       end
 
       def show
+        @profile = current_user.profile
+        render 'api/v1/profile/show'
       end
 
       def user_profile
+        param! :uuid, String, blank: false, required: true
+
+        @profile = User.find_by(uuid: params["uuid"])
+        raise ProfileNotFoundError, I18n.t('api.profile.not_found') unless @profile
+        render 'api/v1/profile/show'
       end
     end
   end
