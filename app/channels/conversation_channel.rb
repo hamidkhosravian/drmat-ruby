d4557c8cbf65df6e3ec8638023fe1352eef701d9
hamid <hamid.khosravian@ut.ac.ir>
2018-04-30 12:55:56 +0430

    update speak

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -11,6 +11,12 @@ class ConversationChannel < ApplicationCable::Channel
   def speak(data)
     body = data['body']
     conversation = Conversation.find_by!(uuid: body['conversation_uid'])
-    Message.create!(body: body['message'], user: current_user, conversation: conversation)
+    if current_user.id == conversation.sender_id or current_user.id == conversation.recipient_id
+      Message.create!(body: body['message'], user: current_user, conversation: conversation)
+    else
+      puts '***************************************************************'
+      puts '*******  CABLE: User cannot join to this conversation  ********'
+      puts '***************************************************************'
+    end
   end
 end
