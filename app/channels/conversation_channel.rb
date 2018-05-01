1e22524150ab6c320cdd9ed1e717ca0457cdaa88
hamid <hamid.khosravian@ut.ac.ir>
2018-05-01 17:32:47 +0430

    add error resend

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -11,26 +11,25 @@ class ConversationChannel < ApplicationCable::Channel
   def conversation(data)
     body = data['body']
     user = User.find_by!(uuid: body['user_uid'])
-    if user.present?
-      conversation = Conversation.create!(sender_id: current_user, recipient_id: user)
-    else
-      # TODO: send to job other ActionCable
-    end
+    conversation = Conversation.create!(sender_id: current_user, recipient_id: user)
+  rescue ActiveRecord::RecordNotFound
+    FailedBroadcastJob.perform_later(current_user, nil, I18n.t('messages.profile.not_found'), "conversations")
   end
 
   def speak(data)
     body = data['body']
     conversation = Conversation.find_by!(uuid: body['conversation_uid'])
     if current_user.id == conversation.sender_id or current_user.id == conversation.recipient_id
-      if conversation.present?
-        Message.create!(body: body['message'], user: current_user, conversation: conversation)
-      elsif !conversation.present?
-        # TODO: send to job other ActionCable
-      end
+      Message.create!(body: body['message'], user: current_user, conversation: conversation)
     else
+      FailedBroadcastJob.perform_later(current_user, conversation, I18n.t('messages.http._401'), "conversations")
+
+      # TODO: remove in production mode
       puts '***************************************************************'
       puts '*******  CABLE: User cannot join to this conversation  ********'
       puts '***************************************************************'
     end
+  rescue ActiveRecord::RecordNotFound
+    FailedBroadcastJob.perform_later(current_user, nil, I18n.t('messages.conversation.not_found'), "conversations")
   end
 end
--- app/jobs/failed_broadcast_job.rb
+++ app/jobs/failed_broadcast_job.rb
@@ -1,7 +1,24 @@
 class FailedBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user, object, error, type)
+    broadcast_to_sender(user, object, error, type)
+  end
+
+  private
+
+  def broadcast_to_sender(user, object, error, type)
+    ActionCable.server.broadcast(
+      "#{type}-#{user.uuid}",
+      error: render_error(user, object, error, type),
+    )
+  end
+
+  def render_error(user, object, error, type)
+    uid = object&.uuid
+    ApplicationController.render(
+      partial: "api/v1/_partials/error",
+      locals: { uid: uid, error: error, type: type}
+    )
   end
 end
